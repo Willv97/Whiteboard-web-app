a1b7baeabf810b2f44c4d50f5223444ca521d8a1
Willv97 <willv1357@gmail.com>
2024-12-08 19:48:19 -0500

    Made eventListeners for when the mose moves up and/or down

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const canvas = document.getElementById('whiteboard');
 const ctx = canvas.getContext('2d');
 
 let isDrawing = false;
+
+canvas.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
+
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+    ctx.beginPath(); //Resets the line for a new stroke
+});
+
+canvas.addEventListener('mousemove', draw);
