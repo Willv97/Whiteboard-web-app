11d3c28ccee9a3f349ee836da62dd9add0c103df
Willv97 <willv1357@gmail.com>
2024-12-08 20:04:35 -0500

    Finished function 'draw'.

--- script.js
+++ script.js
@@ -9,16 +9,26 @@ canvas.addEventListener('mousedown', () => {
 
 canvas.addEventListener('mouseup', () => {
     isDrawing = false;
-    ctx.beginPath(); //Resets the line for a new stroke
+    ctx.beginPath(); // Resets the line for a new stroke
 });
 
 canvas.addEventListener('mousemove', draw);
 
 function draw(event) {
     if (!isDrawing) return;
-}
+
 
 // Get mouse position
 const rect = canvas.getBoundingClientRect();
 const x = event.clientX - rect.left;
 const y = event.clientY - rect.top;
+
+ctx.lineWidth = 2; // Set Thickness
+ctx.lineCap = 'round'; // SRound edges for smoother lines
+ctx.strokeStyle = 'black'; // Line Color
+
+ctx.lineTo(x, y); // Draw to the new point
+ctx.stroke();
+ctx.beginPath(); // Start a new path to continue drawing
+ctx.moveTo(x, y); // Move to the current mouse position
+}
